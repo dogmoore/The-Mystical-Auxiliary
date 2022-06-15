3add83dc5c488efe886820af41c5a81cce7455bf
dogmoore <dogmoore024@gmail.com>
2022-06-15 01:52:00 -0500

    Started work on enemies and planned out some bosses

---
+++ enemies/undead/skeleton.py
@@ -0,0 +1,47 @@
+from user import MainCharacter
+
+# basic magic spells
+# low-level equipment
+
+# drops 1-5 gold
+# drops bones(sold for gold)
+# 20% chance to drop either sword or dagger(not both)
+
+class Skeleton:
+    def __init__(self):
+        pass
+
+    def enemy_type:
+        return 'MINOR UNDEAD'
+
+    def level:
+        pass
+
+    def attack:
+        pass
+
+    def defence:
+        pass
+
+    def inv:
+        pass
+
+    def skills:
+        pass
+
+    def drops:
+        pass
+
+# greater magic spells
+# mid-level equipment
+
+# drops 6-14 gold
+# drops bones(sold for gold)
+# 20% chance to drop either sword or dagger(not both)
+
+class GreaterSkeleton:
+    pass
+
+
+class SuperiorSkeleton:
+    pass
