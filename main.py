1bf6475a411a9fcbfb1d7734d0667285c17886e6
dogmoore <dogmoore024@gmail.com>
2022-06-15 21:34:27 -0500

    Started work on a game window

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import yaml
 import os
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget
-from character_types.characterClass import *
-from saves.loadSaves import load
-from user import MainCharacter
-from userinput import check
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+
+# from character_types.characterClass import *
+# from saves.loadSaves import load
+# from user import MainCharacter
+# from userinput import check
 
 # TODO add color
 # TODO add a confirmation when selecting class along with a short description
@@ -24,38 +25,40 @@ starterTownName = 'Fallburn'
 
 validClasses = ['mage', 'swordsman', 'bowman', 'rogue', 'thief', 'assassin', 'untrained']
 
-charName = input('What is your name young adventurer? ')
-print(f'[A Voice] Welcome young {charName} to the world of Afari. There are many kingdoms that call this continent home.')
-print(f'[A Voice] However, your story begins in a small town called {starterTownName} in the Empire of Moudia. But before that let\'s figure out your class')
 
-characterType = input('[A Voice] There are 7 classes or profession for you to chooose from:\nMage, Swordsman, Bowman, Rogue, Thief, Assassin, or an Untrained: ')
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("The Mystical Auxiliary")
 
-hasChosen = False # checks for valid class type
-if characterType.lower() in validClasses:
-    hasChosen = True
-while(hasChosen == False): 
-    print('[A Voice] So terribly sorry. The world of Afari doesn\'t recognize that as a profession')
-    characterType = input('Please pick either Mage, Swordsman, Bowman, Rogue, Thief, Assassin, or an Untrained: ')
-    if characterType.lower() in validClasses:
-        hasChosen = True
+        self.setGeometry(500, 300, 400, 300)
+        self.startScreen()
+        self.setStyleSheet('background-color: #323233')
 
-gender = input(f'[A Voice] ah a young {characterType}! Now for your gender? ')
+    def startScreen(self):
+        title = QLabel('The Mystical Auxiliary', self)
+        title.move(100, 0)
+        title.setStyleSheet('color: white')
 
-hasChosen = False # checks for valid gender type
-if gender.lower() == 'male' or gender.lower() == 'female':
-    hasChosen = True
-while(hasChosen == False):
-    print('[A Voice] So terribly sorry. The world of Afari ins\'t as advanced as the one you come from')
-    gender = input('Please choose either male or female: ')
-    if gender.lower() == 'male' or gender.lower() == 'female':
-        hasChosen = True
+        cont = QPushButton('Continue', self)
+        cont.setStyleSheet('background-color: #4f4f4f')
+        cont.move(50, 100)
 
-if characterType.lower() == 'mage':
-    print('[A Voice] My name is Ernas and like you I am a Mage for the Moudia Empire')
-else:
-    print('[A Voice] My name is Ernas and I am a Mage for the Moudia Empire')
+        new_game = QPushButton('New Game', self)
+        new_game.setStyleSheet('background-color: #4f4f4f')
+        new_game.move(50, 130)
 
-    #TODO add more story here
+        load_game = QPushButton('Load Game', self)
+        load_game.setStyleSheet('background-color: #4f4f4f')
+        load_game.move(50, 160)
 
-    choice = input(f"Now what will you do {charName}")
-    check('', choice) # needs testing
+        footer = QLabel('Programmed by dogmoore', self)
+        footer.setStyleSheet('color: #4f4f4f')
+        footer.move(140, 280)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
---
+++ story/arc1.py
@@ -0,0 +1,39 @@
+class Arc1:
+    @staticmethod
+    def arc1():
+        pass
+        # charName = input('What is your name young adventurer? ')
+# print(f'[A Voice] Welcome young {charName} to the world of Afari. There are many kingdoms that call this continent home.')
+# print(f'[A Voice] However, your story begins in a small town called {starterTownName} in the Empire of Moudia. But before that let\'s figure out your class')
+#
+# characterType = input('[A Voice] There are 7 classes or profession for you to chooose from:\nMage, Swordsman, Bowman, Rogue, Thief, Assassin, or an Untrained: ')
+#
+# hasChosen = False # checks for valid class type
+# if characterType.lower() in validClasses:
+#     hasChosen = True
+# while(hasChosen == False):
+#     print('[A Voice] So terribly sorry. The world of Afari doesn\'t recognize that as a profession')
+#     characterType = input('Please pick either Mage, Swordsman, Bowman, Rogue, Thief, Assassin, or an Untrained: ')
+#     if characterType.lower() in validClasses:
+#         hasChosen = True
+#
+# gender = input(f'[A Voice] ah a young {characterType}! Now for your gender? ')
+#
+# hasChosen = False # checks for valid gender type
+# if gender.lower() == 'male' or gender.lower() == 'female':
+#     hasChosen = True
+# while(hasChosen == False):
+#     print('[A Voice] So terribly sorry. The world of Afari ins\'t as advanced as the one you come from')
+#     gender = input('Please choose either male or female: ')
+#     if gender.lower() == 'male' or gender.lower() == 'female':
+#         hasChosen = True
+#
+# if characterType.lower() == 'mage':
+#     print('[A Voice] My name is Ernas and like you I am a Mage for the Moudia Empire')
+# else:
+#     print('[A Voice] My name is Ernas and I am a Mage for the Moudia Empire')
+#
+#     #TODO add more story here
+#
+#     choice = input(f"Now what will you do {charName}")
+#     check('', choice) # needs testing
